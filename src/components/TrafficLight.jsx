307f64d966e6cc1c833ed3a0f8f08c6e6d6f7307
KatSultan86 <shelest233@gmail.com>
2025-01-26 21:43:39 -0500

    completed

--- src/components/TrafficLight.jsx
+++ src/components/TrafficLight.jsx
@@ -1,4 +1,23 @@
+
+import { useState } from 'react';
+import '/src/index.css'
+
+
+
+
 export default function TrafficLight() {
+
+    const [isOn, setisOn] = useState(null);
+
+    const toggleLight = (buttonId) => {
+
+        setisOn(buttonId);
+    };
+
+
+
+
+
     return (
 
         <div className="container d-flex justify-content-center align-items-center" style={{ width: "100dvw", height: "100dvh" }}>
@@ -10,18 +29,18 @@ export default function TrafficLight() {
 
 
                 <div className="container flex justify-content-center mt-2">
-                    <div className="light bg-danger"></div>
 
-                    <div className="light bg-warning"></div>
+                    <div className={`light bg-danger ${isOn === 1 ? 'white-shadow' : ''}`} onClick={() => toggleLight(1)} ></div>
+                    <div className={`light bg-warning ${isOn === 2 ? 'white-shadow' : ''}`} onClick={() => toggleLight(2)} ></div>
+                    <div className={`light bg-success ${isOn === 3 ? 'white-shadow' : ''}`} onClick={() => toggleLight(3)} ></div>
 
-                    <div className="light bg-success"></div>
 
                 </div>
 
 
 
             </div>
-        </div>
+        </div >
 
     )
 }
